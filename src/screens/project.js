0ce54bee32cfabaef9ddf90091c92073538070b6
rodrigorodriguesr <rodrigues.rodrigor10@gmail.com>
2025-04-20 21:06:52 -0400

    Change styled page project

--- src/screens/project.js
+++ src/screens/project.js
@@ -1,25 +1,7 @@
 // Estilos de transição para elementos
 const style = document.createElement('style');
 style.innerHTML = `
-    .hidden {
-        opacity: 0;
-        transform: translateY(30px);
-        transition: opacity 0.6s ease-out, transform 0.6s ease-out;
-    }
-    .show {
-        opacity: 1;
-        transform: translateY(0);
-    }
-    #about-container {
-        text-align: center;
-    }
-    #about-container img {
-        width: 15px;
-        height: 15px;
-        border-radius: 50%;
-        object-fit: cover;
-        margin-top: 20px;
-    }
+ 
 `;
 document.head.appendChild(style);
 
